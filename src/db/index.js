c9e6238db434d15d77225895a1a416e4e62a4b38
Le Van Chi <lvchi080306@gmail.com>
2026-05-20 21:13:56 +0700

    feat: initial project setup + DB schema

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+"use strict";
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30000,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected DB error:", err);
+});
+
+module.exports = pool;
